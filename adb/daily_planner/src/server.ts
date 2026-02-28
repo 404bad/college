0085f72d2e93af5cc83dd88d4a10a16067a795ca
kaibad <badukailash001@gmail.com>
2026-02-28 23:54:09 +0545

    Serve static files

--- adb/daily_planner/src/server.ts
+++ adb/daily_planner/src/server.ts
@@ -1,14 +1,24 @@
 import config from "./config/env.config";
 import express, { type Request, type Response } from "express";
 
+import path from "path";
+
 const app = express();
 const PORT = config.PORT || 3000;
 
-console.log("Starting server...");
+// Serve static files
+app.use(express.static(path.join(__dirname, "../public")));
+
+// console.log("Current directory:", __dirname);
+// console.log("Static files directory:", path.join(__dirname, "../public"));
+// console.log(__filename);
+
 app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `Server is running on port ${PORT}. View it at http://localhost:${PORT}`,
+  );
 });
