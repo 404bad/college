c68365948eeadc3a2d0207e9514ec3f2923500b4
kaibad <badukailash001@gmail.com>
2026-03-01 00:38:21 +0545

    feat: connect to MongoDB and add graceful shutdown
    - Integrate Mongoose for MongoDB Atlas connection
    - Add SIGTERM and SIGINT signal handlers for graceful shutdown
    - Close HTTP server and MongoDB connection on shutdown

--- adb/daily_planner/src/config/env.config.ts
+++ adb/daily_planner/src/config/env.config.ts
@@ -4,10 +4,12 @@ dotenv.config();
 
 interface configProps {
   PORT: string | number;
+  MONGO_URI: string;
 }
 
 const config: configProps = {
   PORT: process.env.PORT || 3000,
+  MONGO_URI: process.env.MONGO_URI || "",
 };
 
 export default config;
--- adb/daily_planner/src/server.ts
+++ adb/daily_planner/src/server.ts
@@ -1,11 +1,15 @@
 import config from "./config/env.config";
 import express, { type Request, type Response } from "express";
-
 import path from "path";
+
+import { connectDB, disconnectDB } from "./config/db.config";
 
 const app = express();
 const PORT = config.PORT || 3000;
 
+//middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Serve static files
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -13,12 +17,26 @@ app.use(express.static(path.join(__dirname, "../public")));
 // console.log("Static files directory:", path.join(__dirname, "../public"));
 // console.log(__filename);
 
+connectDB();
+
 app.get("/", (_req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.listen(PORT, () => {
+const Server = app.listen(PORT, () => {
   console.log(
     `Server is running on port ${PORT}. View it at http://localhost:${PORT}`,
   );
 });
+
+process.on("SIGTERM", shutdown);
+process.on("SIGINT", shutdown);
+
+async function shutdown() {
+  console.log("Shutting down server...");
+
+  Server.close(async () => {
+    console.log("HTTP server closed.");
+    await disconnectDB();
+  });
+}
